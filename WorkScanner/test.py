33d93b84daed07bb13c4977860d1cb06f263e0db
jason <jason@jbrmmg.me.uk>
2022-04-25 21:49:52 +0100

    help solve wordle

--- WorkScanner/test.py
+++ WorkScanner/test.py
@@ -1,126 +1,178 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import font
+
+class PlaceFlags(tk.Frame):
+    def __init__(self, parent, include):
+        tk.Frame.__init__(self, parent)
+
+        self.colour = "#ffcc80"
+        if(include == 1):
+            self.colour = "#99ffbb"
+
+        self.enabled = tk.IntVar(value=1)
+        self.flags = [0 for x in range(5)]
+        self.values = [0 for x in range(5)]
+        for x in range(5):
+            self.values[x] = tk.IntVar(value=0)
+            self.flags[x] = tk.Checkbutton(self, variable=self.values[x], selectcolor=self.colour)
+            self.flags[x].grid(row=x, column=0)
+
+    def Enable(self):
+        for x in range(5):
+            if(self.enabled.get() == 1):
+                self.flags[x].config(state=tk.DISABLED)
+                self.values[x].set(0)
+            else:
+                self.flags[0].config(state=tk.NORMAL)
+        if(self.enabled.get() == 1):
+            self.enabled.set(0)
+        else:
+            self.enabled.set(1)
+
+    def Pattern(self, letter):
+        result = ""
+
+        for x in range(5):
+            if(self.values[x].get() == 1):
+                result = result + letter
+            else:
+                result = result + "_"
+
+        return result
+
+    def IsUsed(self, letter):
+        for x in range(5):
+            if(self.values[x].get() == 1):
+                return letter
+
+        return ""
+
+    def Reset(self):
+        for x in range(5):
+            self.values[x].set(0)
+
+    def Include(self, letter, index):
+        if(self.values[index].get() == 1):
+            return letter
+
+        return ""
 
 
-class YesOrControl(Frame):
-    def __init__(self, parent, yesorno=bool(0)):
-        Frame.__init__(self, parent)
+class LetterWidget(tk.Frame):
+    def __init__(self, parent, index):
+        tk.Frame.__init__(self, parent)
 
-        color = "#ffd699"
-        if yesorno:
-            color = "#80ffcc"
-
-        c1 = IntVar()
-        self.chk1 = Checkbutton(self, variable=c1, selectcolor=color)
-        c2 = IntVar()
-        self.chk2 = Checkbutton(self, variable=c2, selectcolor=color)
-        c3 = IntVar()
-        self.chk3 = Checkbutton(self, variable=c3, selectcolor=color)
-        c4 = IntVar()
-        self.chk4 = Checkbutton(self, variable=c4, selectcolor=color)
-        c5 = IntVar()
-        self.chk5 = Checkbutton(self, variable=c5, selectcolor=color)
-
-        self.chk1.grid(row=0, column=0, sticky="ew")
-        self.chk2.grid(row=1, column=0, sticky="ew")
-        self.chk3.grid(row=2, column=0, sticky="ew")
-        self.chk4.grid(row=3, column=0, sticky="ew")
-        self.chk5.grid(row=4, column=0, sticky="ew")
-
-
-class YesNoControl(Frame):
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
-
-        self.yes = YesOrControl(self, bool(1))
-        self.no = YesOrControl(self, bool(0))
-
-        self.yes.grid(row=0, column=0, sticky="ns")
-        self.no.grid(row=0, column=1, sticky="ns")
-
-
-class LetterControl(Frame):
-    def __init__(self, parent, letter="A"):
-        Frame.__init__(self, parent)
-
-        self.label = Label(self, text=letter, anchor="w", font=("Arial", 32))
-        c = IntVar(value=1)
-        c.set(1)
-        self.select = Checkbutton(self, variable=c)
-        self.select.select()
-        self.yesNo = YesNoControl(self)
-
+        self.label = tk.Label(self, text=chr(65+index), font=("Ariel", 32))
         self.label.grid(row=0, column=0, columnspan=2)
-        self.select.grid(row=1, column=0, columnspan=2)
-        self.yesNo.grid(row=2, column=1)
+
+        self.CheckVar = tk.IntVar(value=1)
+        self.cb = tk.Checkbutton(self, variable=self.CheckVar, command=self.Enable)
+        self.cb.grid(row=1, column=0, columnspan=2)
+
+        self.includeFlags = PlaceFlags(self,1)
+        self.includeFlags.grid(row=2, column=0)
+
+        self.excludeFlags = PlaceFlags(self,0)
+        self.excludeFlags.grid(row=2, column=1)
+
+    def Enable(self):
+        self.includeFlags.Enable()
+        self.excludeFlags.Enable()
+
+    def IsEnabled(self):
+        if(self.CheckVar.get() == 0):
+            return self.label.cget("text")
+
+        return ""
+
+    def Reset(self):
+        self.CheckVar.set(1)
+        self.includeFlags.Reset()
+        self.excludeFlags.Reset()
+
+    def IsUsed(self):
+        return self.excludeFlags.IsUsed(self.label.cget("text"))
+
+    def ExcludePattern(self):
+        return self.excludeFlags.Pattern(self.label.cget("text"))
+
+    def Include(self,index):
+        return self.includeFlags.Include(self.label.cget("text"), index)
 
 
-class Example(Frame):
+class MainFrame(tk.Frame):
     def __init__(self, parent):
-        Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent)
 
-        self.l1 = LetterControl(self, "A")
-        self.l2 = LetterControl(self, "B")
-        self.l3 = LetterControl(self, "C")
-        self.l4 = LetterControl(self, "D")
-        self.l5 = LetterControl(self, "E")
-        self.l6 = LetterControl(self, "F")
-        self.l7 = LetterControl(self, "G")
-        self.l8 = LetterControl(self, "H")
-        self.l9 = LetterControl(self, "I")
-        self.l10 = LetterControl(self, "J")
-        self.l11 = LetterControl(self, "K")
-        self.l12 = LetterControl(self, "L")
-        self.l13 = LetterControl(self, "M")
-        self.l14 = LetterControl(self, "N")
-        self.l15 = LetterControl(self, "O")
-        self.l16 = LetterControl(self, "P")
-        self.l17 = LetterControl(self, "Q")
-        self.l18 = LetterControl(self, "R")
-        self.l19 = LetterControl(self, "S")
-        self.l20 = LetterControl(self, "T")
-        self.l21 = LetterControl(self, "U")
-        self.l22 = LetterControl(self, "V")
-        self.l23 = LetterControl(self, "W")
-        self.l24 = LetterControl(self, "X")
-        self.l25 = LetterControl(self, "Y")
-        self.l26 = LetterControl(self, "Z")
-        self.list = Listbox(self)
+        self.letter = [0 for x in range(26)]
+        for x in range(26):
+            self.letter[x] = LetterWidget(self, x)
+            self.letter[x].grid(row=0, column=x)
 
-        self.l1.grid(row=0, column=0, sticky="ns")
-        self.l2.grid(row=0, column=1, sticky="ns")
-        self.l3.grid(row=0, column=2, sticky="ns")
-        self.l4.grid(row=0, column=3, sticky="ns")
-        self.l5.grid(row=0, column=4, sticky="ns")
-        self.l6.grid(row=0, column=5, sticky="ns")
-        self.l7.grid(row=0, column=6, sticky="ns")
-        self.l8.grid(row=0, column=7, sticky="ns")
-        self.l9.grid(row=0, column=8, sticky="ns")
-        self.l10.grid(row=0, column=9, sticky="ns")
-        self.l11.grid(row=0, column=10, sticky="ns")
-        self.l12.grid(row=0, column=11, sticky="ns")
-        self.l13.grid(row=0, column=12, sticky="ns")
-        self.l14.grid(row=0, column=13, sticky="ns")
-        self.l15.grid(row=0, column=14, sticky="ns")
-        self.l16.grid(row=0, column=15, sticky="ns")
-        self.l17.grid(row=0, column=16, sticky="ns")
-        self.l18.grid(row=0, column=17, sticky="ns")
-        self.l19.grid(row=0, column=18, sticky="ns")
-        self.l20.grid(row=0, column=19, sticky="ns")
-        self.l21.grid(row=0, column=20, sticky="ns")
-        self.l22.grid(row=0, column=21, sticky="ns")
-        self.l23.grid(row=0, column=22, sticky="ns")
-        self.l24.grid(row=0, column=23, sticky="ns")
-        self.l25.grid(row=0, column=24, sticky="ns")
-        self.l26.grid(row=0, column=25, sticky="ns")
-        self.list.grid(row=1, column=0, columnspan=26, sticky="nsew", pady=10, padx=5)
+        self.trigger = tk.Button(self, text="Evaluate", command=self.Evaluate)
+        self.trigger.grid(row=1, column=0, columnspan=4)
 
-        self.columnconfigure(index=0, weight=0)
-        self.rowconfigure(index=1, weight=1)
+        self.reset = tk.Button(self, text="Reset", command=self.Reset)
+        self.reset.grid(row=1, column=4, columnspan=4)
+
+        listFont = font.Font(family="Courier", size=10)
+        self.list = tk.Listbox(self, font=listFont)
+        self.list.grid(row=2, column=0, columnspan=26, sticky="nsew", padx=5, pady=5)
+
+        self.columnconfigure(0,weight=1)
+        self.rowconfigure(2,weight=1)
+
+        self.scroll = tk.Scrollbar(self.list, orient=tk.VERTICAL)
+        self.scroll.pack(side=tk.RIGHT,fill="y")
+        self.list.config(yscrollcommand=self.scroll.set)
+        self.scroll.config(command=self.list.yview)
+
+    def Reset(self):
+        self.list.delete(0, tk.END)
+
+        for x in range(26):
+            self.letter[x].Reset()
+
+    def Evaluate(self):
+        self.list.delete(0, tk.END)
+
+        unsed = ""
+        for x in range(26):
+            unused = unsed + self.letter[x].IsEnabled()
+        self.list.insert(tk.END, unused)
+
+        used = ""
+        for x in range(26):
+            used = used + self.letter[x].IsUsed()
+        self.list.insert(tk.END, used)
+
+        fullExcludePattern = ""
+        for x in range(26):
+            excludePattern = self.letter[x].ExcludePattern()
+            if(excludePattern != "_____"):
+                fullExcludePattern = fullExcludePattern + excludePattern + " "
+        self.list.insert(tk.END, fullExcludePattern)
+
+        includePattern = ""
+        for x in range(5):
+            include = ""
+            for y in range(26):
+                include = self.letter[y].Include(x)
+                if(include != ""):
+                    break
+
+                if(include != ""):
+                    includePattern = includePattern + include
+                else:
+                    includePattern = includePattern + "_"
+        self.list.insert(tk.END, includePattern)
+
+        for x in range(40):
+            self.list.insert(tk.END, "EARTH EMOJI WITCH FRAME CHAUX PLANE")
 
 
 if __name__ == "__main__":
-    root = Tk()
-    root.title("Scanner")
-    root.geometry("1420x600")
-    Example(root).place(x=0, y=0, relwidth=1, relheight=1)
-    root.mainloop()
+    window = tk.Tk()
+    window.geometry("1460x800")
+    MainFrame(window).place(x=0, y=0, relwidth=1, relheight=1)
+    window.mainloop()
