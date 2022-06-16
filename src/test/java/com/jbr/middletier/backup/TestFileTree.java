393b9f492537187d75816eb69359552dc830b93e
jason <jason@jbrmmg.me.uk>
2022-06-16 17:44:31 +0100

    JBR-291 refactoring - synchronisation

--- src/test/java/com/jbr/middletier/backup/TestFileTree.java
+++ src/test/java/com/jbr/middletier/backup/TestFileTree.java
@@ -158,6 +158,91 @@ public class TestFileTree {
         }
     }
 
+    private static class BasicDbTree extends DbTree {
+        public BasicDbTree() {
+            super(null, null);
+        }
+
+        public boolean test() {
+            DbDirectory mockDbDirectory = mock(DbDirectory.class);
+            when(mockDbDirectory.isDirectory()).thenReturn(true);
+
+            FileTreeNode test = createCompareNode(CompareStatusType.REMOVED, null, mockDbDirectory, null);
+            Assert.assertNotNull(test);
+
+            try {
+                createCompareNode(CompareStatusType.REMOVED, null, null, null);
+                Assert.fail();
+            } catch(IllegalStateException e) {
+                Assert.assertEquals("Status is removed, but no source provided", e.getMessage());
+            }
+
+            test = createCompareNode(CompareStatusType.ADDED, null, null, mockDbDirectory);
+            Assert.assertNotNull(test);
+
+            try {
+                createCompareNode(CompareStatusType.ADDED, null, null, null);
+                Assert.fail();
+            } catch(IllegalStateException e) {
+                Assert.assertEquals("Status is added, but no destination provided", e.getMessage());
+            }
+
+            DbCompareNode node = mock(DbCompareNode.class);
+            when(node.getActionType()).thenReturn(DbCompareNode.ActionType.REMOVE);
+            when(node.isDirectory()).thenReturn(false);
+
+            List<FileTreeNode> list = new ArrayList<>();
+
+            findDeleteFiles(node, list);
+            Assert.assertEquals(1, list.size());
+            list.clear();
+
+            when(node.getActionType()).thenReturn(DbCompareNode.ActionType.RECREATE_AS_DIRECTORY);
+            when(node.isDirectory()).thenReturn(true);
+            findDeleteFiles(node, list);
+            Assert.assertEquals(1, list.size());
+            list.clear();
+
+            when(node.getActionType()).thenReturn(DbCompareNode.ActionType.REMOVE);
+            when(node.isDirectory()).thenReturn(true);
+            findDeleteDirectories(node, list);
+            Assert.assertEquals(1, list.size());
+            list.clear();
+
+            when(node.getActionType()).thenReturn(DbCompareNode.ActionType.RECREATE_AS_FILE);
+            when(node.isDirectory()).thenReturn(false);
+            findDeleteDirectories(node, list);
+            Assert.assertEquals(1, list.size());
+            list.clear();
+
+            when(node.getActionType()).thenReturn(DbCompareNode.ActionType.COPY);
+            when(node.isDirectory()).thenReturn(true);
+            findInsertDirectories(node, list);
+            Assert.assertEquals(1, list.size());
+            list.clear();
+
+            when(node.getActionType()).thenReturn(DbCompareNode.ActionType.RECREATE_AS_DIRECTORY);
+            when(node.isDirectory()).thenReturn(false);
+            findInsertDirectories(node, list);
+            Assert.assertEquals(1, list.size());
+            list.clear();
+
+            when(node.getActionType()).thenReturn(DbCompareNode.ActionType.COPY);
+            when(node.isDirectory()).thenReturn(false);
+            findInsertFiles(node, list);
+            Assert.assertEquals(1, list.size());
+            list.clear();
+
+            when(node.getActionType()).thenReturn(DbCompareNode.ActionType.RECREATE_AS_FILE);
+            when(node.isDirectory()).thenReturn(false);
+            findInsertFiles(node, list);
+            Assert.assertEquals(1, list.size());
+            list.clear();
+
+            return true;
+        }
+    }
+
     private static class BasicSection extends SectionNode {
 
         public BasicSection(SectionNodeType section) {
@@ -434,5 +519,8 @@ public class TestFileTree {
         result = test.getOrderedNodeList();
         Assert.assertNotNull(result);
         Assert.assertEquals(7, result.size());
+
+        BasicDbTree basicDbTree = new BasicDbTree();
+        Assert.assertTrue(basicDbTree.test());
     }
 }
