9dbf2cef842d01f1e2b22fa79f76f0784d7149f6
jason <jason@jbrmmg.me.uk>
2022-06-28 21:32:54 +0100

    JBR-291 refactoring - improve test coverage

--- src/main/java/com/jbr/middletier/backup/filetree/RootFileTreeNode.java
+++ src/main/java/com/jbr/middletier/backup/filetree/RootFileTreeNode.java
@@ -1,16 +1,10 @@
 package com.jbr.middletier.backup.filetree;
 
-import java.util.List;
-
 public abstract class RootFileTreeNode extends FileTreeNode {
 
     protected RootFileTreeNode() {
         super(null);
     }
-
-//    protected List<FileTreeNode> getChildren(FileTreeNode parent) {
-//        return parent.children;
-//    }
 
     @Override
     public String toString() {
