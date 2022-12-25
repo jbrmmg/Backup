4e61812fc10eeeae1f0000ccd5704eb03bd99ec0
jason <jason@jbrmmg.me.uk>
2022-12-25 08:45:49 +0000

    Select photos for print

--- src/main/java/com/jbr/middletier/backup/dataaccess/FileRepository.java
+++ src/main/java/com/jbr/middletier/backup/dataaccess/FileRepository.java
@@ -2,7 +2,9 @@ package com.jbr.middletier.backup.dataaccess;
 
 import com.jbr.middletier.backup.data.FileInfo;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +14,6 @@ public interface FileRepository extends CrudRepository<FileInfo, Integer>, JpaSp
     Iterable<FileInfo> findAllByOrderByIdAsc();
 
     Iterable<FileInfo> findByParentId(Integer parentId);
+
+    Iterable<FileInfo> findByFlagsContaining(String flags);
 }
