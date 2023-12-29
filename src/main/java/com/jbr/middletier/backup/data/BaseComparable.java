5be60d0b9913413fcf50ff7bb47315c445148707
jason <jason@jbrmmg.me.uk>
2023-12-29 14:46:35 +0000

    JBR-395 clean up and testing.

--- src/main/java/com/jbr/middletier/backup/data/BaseComparable.java
+++ src/main/java/com/jbr/middletier/backup/data/BaseComparable.java
@@ -2,12 +2,12 @@ package com.jbr.middletier.backup.data;
 
 import java.io.Serializable;
 
-public abstract class BaseComparable implements Serializable {
+public class BaseComparable implements Serializable {
     @Override
     public boolean equals(Object o) {
         if(o == this) return true;
 
-        if(!o.getClass().equals(this.getClass())) {
+        if(o == null || !o.getClass().equals(this.getClass())) {
             return false;
         }
 
