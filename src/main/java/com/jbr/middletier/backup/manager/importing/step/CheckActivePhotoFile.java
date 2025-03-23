4de6bd8bbdcc06e7847942f6e57465aef5462f2c
jason <jason@jbrmmg.me.uk>
2025-03-23 20:00:10 +0000

    JBR-537 improving the import process

--- src/main/java/com/jbr/middletier/backup/manager/importing/step/CheckActivePhotoFile.java
+++ src/main/java/com/jbr/middletier/backup/manager/importing/step/CheckActivePhotoFile.java
@@ -1,8 +1,10 @@
 package com.jbr.middletier.backup.manager.importing.step;
 
+import com.jbr.middletier.backup.data.IgnoreFile;
 import com.jbr.middletier.backup.data.ImportFile;
 import com.jbr.middletier.backup.data.TrafficLightType;
 import com.jbr.middletier.backup.dataaccess.ImportFileRepository;
+import com.jbr.middletier.backup.dto.ImportFileBaseDTO;
 import com.jbr.middletier.backup.dto.PreImportFileDTO;
 import com.jbr.middletier.backup.manager.AssociatedFileDataManager;
 import com.jbr.middletier.backup.manager.importing.FileProcessingStepType;
@@ -26,14 +28,55 @@ public class CheckActivePhotoFile extends ImportStep {
         return FileProcessingStepType.FPS_CHECK_ACTIVE_PHOTO_FILE;
     }
 
+    private ImportFileBaseDTO getSimilarFile(ImportFile importFile) {
+        ImportFileBaseDTO similar = new ImportFileBaseDTO();
+        similar.setFilename(importFile.getName());
+        similar.setDate(importFile.getDate());
+        similar.setSize(importFile.getSize());
+        similar.setMd5(importFile.getMD5());
+
+        return similar;
+    }
+
+    private boolean isWriteTypeForActivePhoto(PreImportFileDTO file) {
+        return file.getFilename().toLowerCase().endsWith(".mov") && file.getImportName().toLowerCase().endsWith(".mp4");
+    }
+
+    private String getNameWithNoExtension(String fileName) {
+        return fileName.substring(0, fileName.lastIndexOf("."));
+    }
+
     @Override
     public TrafficLightType performStep(PreImportFileDTO file) {
         LOG.info("Checking active photo file");
-        return TrafficLightType.TL_RED;
+        // This check only applies to a file that ends MOV and the import is mp4.
+        if(!isWriteTypeForActivePhoto(file)) {
+            return TrafficLightType.TL_GREEN;
+        }
+
+        // Active photo is an MOV/mp4 that has the same name and the mp4 will have a close date.
+        for(ImportFile importFile : importFileRepository.findAll()) {
+            if(importFile.getName().equalsIgnoreCase(file.getFilename())) {
+                continue;
+            }
+
+            // Does this match on the name.
+            String importFilename = getNameWithNoExtension(importFile.getName());
+            String fileFilename = getNameWithNoExtension(file.getFilename());
+
+            if(importFilename.equalsIgnoreCase(fileFilename)) {
+                // Add this as a similar file.
+                file.addSimilarFile(getSimilarFile(importFile));
+                return TrafficLightType.TL_RED;
+            }
+        }
+
+        return TrafficLightType.TL_GREEN;
     }
 
     @Override
     protected boolean transferData(PreImportFileDTO file, ImportFile record) {
+        // This does not get stored.
         return false;
     }
 }
