b70da53af30fb9a28a18e81ff9efda5ee6d21046
jbrmmg <jason@jbrmmg.me.uk>
2026-08-19 07:32:58 +0100

    Fix error deleting backup runs

--- src/main/java/com/jbr/middletier/backup/dataaccess/BackupJobRunRepository.java
+++ src/main/java/com/jbr/middletier/backup/dataaccess/BackupJobRunRepository.java
@@ -4,6 +4,7 @@ import com.jbr.middletier.backup.data.BackupJobRun;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,5 +15,6 @@ public interface BackupJobRunRepository extends CrudRepository<BackupJobRun, Lon
     Optional<BackupJobRun> findByBackupIdAndRunDate(String backupId, LocalDate runDate);
     List<BackupJobRun> findAllByOrderByRunDateDescStartedAtAsc();
     List<BackupJobRun> findByBackupIdOrderByRunDateDesc(String backupId, Pageable pageable);
+    @Transactional
     void deleteByRunDateBefore(LocalDate cutoff);
 }
